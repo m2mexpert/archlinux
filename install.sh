cb3744bcb468b18b4a182bded30af8f583f018a5
m2mexpert <m2mexpert@users.noreply.github.com>
2021-08-04 18:02:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-# This is Krushn's Arch Linux Installation Script.
-# Visit krushndayshmookh.github.io/krushn-arch for instructions.
-
-echo "Krushn's Arch Installer"
+echo "Joe's Way Cool Arch Installer"
 
 # Set up network connection
 read -p 'Are you connected to internet? [y/N]: ' neton
@@ -73,7 +70,7 @@ swapon /dev/vda2
 # Install Arch Linux
 echo "Starting install.."
 echo "Installing Arch Linux, KDE with Konsole and Dolphin and GRUB2 as bootloader" 
-pacstrap /mnt base base-devel zsh grml-zsh-config grub os-prober intel-ucode efibootmgr dosfstools freetype2 fuse2 mtools iw wpa_supplicant dialog xorg xorg-server xorg-xinit mesa xf86-video-intel plasma konsole dolphin
+pacstrap /mnt base base-devel zsh grml-zsh-config grub os-prober intel-ucode efibootmgr dosfstools dhcpcd freetype2 fuse2 mtools iw wpa_supplicant dialog xorg xorg-server xorg-xinit mesa xf86-video-intel plasma konsole dolphin
 
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab

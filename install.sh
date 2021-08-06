a62b07065c396ad63cc093e8f1d8676653c3dc10
m2mexpert <m2mexpert@users.noreply.github.com>
2021-08-05 22:48:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+printf "\033c"
 echo "Joe's Way Cool Arch Installer"
 
 # Set up network connection
@@ -82,6 +83,7 @@ chmod a+x /mnt/root/post-install.sh
 arch-chroot /mnt /root/post-install.sh
 
 # Finish
+printf "\033c"
 echo "If post-install.sh was run succesfully, you will now have a fully working bootable Arch Linux system installed."
 echo "The only thing left is to reboot into the new system."
 echo "Press any key to reboot or Ctrl+C to cancel..."
